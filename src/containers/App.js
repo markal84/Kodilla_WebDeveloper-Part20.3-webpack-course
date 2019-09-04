64200099f821521e3f7738d54675eb3e0c90f04d
Marcin Kaleta <kalet1984@gmail.com>
2019-09-04 15:56:07 +0200

    Finish installing and configuring plugins

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,7 +8,20 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            data: []
+            data: [
+                {
+                id: 1,
+                text: 'clean room'
+                }, 
+                {
+                id: 2,
+                text: 'wash the dishes'
+                }, 
+                {
+                id: 3,
+                text: 'feed my cat'
+                }
+            ]
         };
     }
     addTodo(val){
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const plugins = [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})];
 
 module.exports = env => {
+
+    if (env === 'production') {
+        plugins.push(
+            new OptimizeJsPlugin({
+                sourceMap: false
+            })
+        )
+    }
 
     const environment = env || 'production' || 'development';
 
@@ -35,6 +50,8 @@ module.exports = env => {
                     ]
                 }
             ]
-        }
+        },
+
+        plugins: plugins
     }
 };
